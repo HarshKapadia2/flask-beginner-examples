b828699f61340eca9084022f78d48739ac614391
Harsh Kapadia <harshgkapadia@gmail.com>
2020-05-19 04:37:29 +0530

    :sparkles: feat: Add example_4

---
+++ example_4/example_4.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, flash, redirect
+
+app = Flask(__name__)
+
+
+
+@app.route('/', methods=['GET'])
+def home_page():
+        return render_template('home_page.html')
+
+
+@app.route('/dashboard', methods=['POST'])
+def dashboard():
+    if request.method == 'POST':
+        f_name = request.form['f-name']
+        l_name = request.form['l-name']
+        age = request.form['age']
+
+        return render_template('dashboard.html', f_name = f_name, l_name = l_name, age = age)
+
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
